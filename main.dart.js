49713b99800f6e494e2a87b9d2a45c5a165bd32e
benjamoide <benjamoide@users.noreply.github.com>
2026-02-12 17:48:05 +0000

    deploy: c375de12ca9bc15b281b7c2a77c1551a720f0971

--- main.dart.js
+++ main.dart.js
@@ -97813,14 +97813,14 @@ if(g===630)n=f
 else if(g===660)m=f
 else if(g===810)l=f
 else if(g===830)k=f
-else if(g===850)j=f}a[0]=n
-a[1]=m
-a[2]=l
+else if(g===850)j=f}a[0]=0
+a[1]=0
+a[2]=n
 a[3]=m
 a[4]=l
 a[5]=k
 a[6]=j
-A.dQ("BLE: Sending Brightness (Shotgun v46): "+A.m(a))
+A.dQ("BLE: Sending Brightness (Offset v47): "+A.m(a))
 p=q.z
 s=2
 return A.q(p.dL(A.aK4(a)),$async$m0)
