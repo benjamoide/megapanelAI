c8839ec6bd2a83b8706a937221ce262bad501e62
benjamoide <benjamoide@users.noreply.github.com>
2026-05-20 10:46:12 +0000

    deploy: e744af5c5e1bc9578e9714b17b68fd8c6753b6e7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2946854396" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1747377919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
